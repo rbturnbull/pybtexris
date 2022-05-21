2473541929d5c4ca193847a22d168e33e27677d5
Robert Turnbull <robert.turnbull@unimelb.edu.au>
2022-05-21 15:47:52 +1000

    :cop: adding more tests and adding files required

--- tests/test_format.py
+++ tests/test_format.py
@@ -1,17 +1,60 @@
 from pathlib import Path
-from pybtex import PybtexEngine
+from pybtex import format_from_files, format_from_file
+
+shannon1948 = "[1] Claude E. Shannon. A mathematical theory of communication. Bell System Technical Journal, 27:379–423, July 1948.\n"
+files_dir = Path(__file__).parent / "files"
 
 
-def test_format_plain():
-    style="plain"
-    output_backend="plaintext"
-    engine = PybtexEngine()
-    file = Path(__file__).parent/"files/Shannon1948.ris"
-    result = engine.format_from_files(
-        bib_files_or_filenames=[file], 
-        style=style, 
-        output_backend=output_backend,
+def test_format_from_files():
+    result = format_from_files(
+        [files_dir/"Shannon1948.ris"], 
+        style="plain", 
+        output_backend="plaintext",
+        bib_format="ris",
+    )
+    assert result == shannon1948
+
+
+def test_format_from_file():
+    result = format_from_file(
+        files_dir/"Shannon1948.ris", 
+        style="plain", 
+        output_backend="plaintext",
+        bib_format="ris",
+    )
+    assert result == shannon1948
+
+
+def test_format_from_files_suffix():
+    result = format_from_files(
+        [files_dir/"Shannon1948.ris"], 
+        style="plain", 
+        output_backend="plaintext",
         bib_format="suffix",
     )
-    assert result == "[1] Claude E. Shannon. A mathematical theory of communication. Bell System Technical Journal, 27:379–423, July 1948.\n"
+    assert result == shannon1948
+
+
+def test_format_from_file_suffix():
+    result = format_from_file(
+        files_dir/"Shannon1948.ris", 
+        style="plain", 
+        output_backend="plaintext",
+        bib_format="suffix",
+    )
+    assert result == shannon1948
+
+
+def test_format_from_files_suffix_multi():
+    result = format_from_files(
+        [files_dir/"Shannon1948.ris", files_dir/"Knuth1986.bib"], 
+        style="plain", 
+        output_backend="plaintext",
+        bib_format="suffix",
+    )
+    expected = (
+        "[1] Donald E. Knuth. The \TeX  Book. Addison-Wesley Professional, 1986.\n"
+        "[2] Claude E. Shannon. A mathematical theory of communication. Bell System Technical Journal, 27:379–423, July 1948.\n"
+    )
+    assert result == expected
 
