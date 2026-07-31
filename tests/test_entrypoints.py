b42c67331d324312edc93a7d58b467022e36fee0
Robert Turnbull <robert.turnbull@unimelb.edu.au>
2026-07-31 15:40:54 +1000

    :zap: getting to work with latest pybtex

--- tests/test_entrypoints.py
+++ tests/test_entrypoints.py
@@ -1,22 +1,26 @@
-import pkg_resources
+from importlib.metadata import entry_points
+
 from pybtexris import RISParser
+
+
+def get_entry_points(group):
+    discovered = entry_points()
+    if hasattr(discovered, "select"):
+        return discovered.select(group=group)
+    return discovered.get(group, ())
+
 
 def test_database_input():
     hook = "pybtex.database.input"
-    ris_entry_points = []
-    for entry_point in pkg_resources.iter_entry_points(hook):
-        if entry_point.name == "ris":
-            ris_entry_points.append(entry_point)
+    ris_entry_points = [entry_point for entry_point in get_entry_points(hook) if entry_point.name == "ris"]
 
     assert len(ris_entry_points) == 1
     assert ris_entry_points[0].load() == RISParser
 
+
 def test_database_input_suffixes():
     hook = "pybtex.database.input.suffixes"
-    ris_entry_points = []
-    for entry_point in pkg_resources.iter_entry_points(hook):
-        if entry_point.name == ".ris":
-            ris_entry_points.append(entry_point)
+    ris_entry_points = [entry_point for entry_point in get_entry_points(hook) if entry_point.name == ".ris"]
 
     assert len(ris_entry_points) == 1
     assert ris_entry_points[0].load() == RISParser
